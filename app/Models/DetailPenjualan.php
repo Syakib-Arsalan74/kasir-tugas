f1d02f0fe24e127eb83a2aa7d30d11fd776bc50f
Syakib-Arsalan74 <syakibgithub@gmail.com>
2025-02-19 07:42:58 +0700

    menambah Schema Database

---
+++ app/Models/DetailPenjualan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DetailPenjualan extends Model
+{
+    /** @use HasFactory<\Database\Factories\DetailPenjualanFactory> */
+    use HasFactory;
+}
